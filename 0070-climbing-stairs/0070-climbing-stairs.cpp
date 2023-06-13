7062b65c5f4e89f2044e2702c4a1c455b755d66e
Syed-Raheem <91645892+Syed-Raheem@users.noreply.github.com>
2023-06-13 18:03:07 +0530

    Time: 4 ms (12.42%), Space: 6.1 MB (36.46%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        // if(n==1)
+        // {
+        //     return 1;
+        // }
+        // else if(n==2)
+        // {
+        //     return 2;
+        // }
+        // else if(n==3)
+        // {
+        //     return 3;
+        // }
+        // return climbStairs(n-1)+climbStairs(n-2);
+        vector<int> v;
+        v.push_back(1);
+        v.push_back(2);
+        v.push_back(3);
+        for(int i=3;i<n;i++)
+        {
+            int t=v[i-1]+v[i-2];
+            v.push_back(t);
+        }
+        return v[n-1];
+    }
+};
