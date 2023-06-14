e6e6df4b9a26c6342f10630f5b4accdfff005832
Syed-Raheem <91645892+Syed-Raheem@users.noreply.github.com>
2023-06-14 17:04:54 +0530

    Time: 4 ms (47.89%), Space: 6.6 MB (79.41%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int longestPalindrome(string s) 
+    {
+        map<char,int> mp;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+        }
+        
+        int c=0,k=0;
+        for(auto i: mp)
+        {
+            cout<<i.first<<" "<<i.second<<"\n";
+        }
+        for(auto i : mp)
+        {
+            if(i.second%2 == 0)
+            {
+                c=c+i.second;
+            }
+            else
+            {
+                if(i.second>1 && k==0)
+                {
+                    c=c+i.second;
+                    k++;
+                }
+                else if(i.second==1 && k==0)
+                {
+                    c=c+1;
+                    k++;
+                }
+                else
+                {
+                    c=c+(i.second-1);
+                }
+            }
+        }
+        return c;
+    }
+};
